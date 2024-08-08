f3501de763cb6d807318edd39e3ef854c0459b7c
Evgeniy <defender35185@gmail.com>
2024-08-08 09:48:30 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let length = str.length / 2
+  for (let i = 0; i < length; i++) {
+    bracketsConfig.forEach(arr => {
+      str = str.replace(`${arr[0]}${arr[1]}`, '')
+    })
+  }
+  return !str.length
 }
